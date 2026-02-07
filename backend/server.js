7d18049caaa486426f802d76b468f126b387e555
Mahmoud Rabea <bodamomo2010@gmail.com>
2026-02-07 11:30:52 +0200

    fix api paths for production

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ app.use(express.urlencoded({ extended: true, limit: "20mb" }));
 app.use(cookieParser()); 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin:true,
     credentials: true,
   })
 );
